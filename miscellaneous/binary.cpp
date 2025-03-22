72ca53e90381e7430d0234dcfffa072bfe4bc1d1
Steve <steve.chen@mail.mcgill.ca>
2025-03-22 23:16:14 +0000

    alternative implementation

--- miscellaneous/binary.cpp
+++ miscellaneous/binary.cpp
@@ -2,6 +2,23 @@
 #include <string>
 using namespace std;
 
+int binaryToInt(const string& binaryStr) {
+    int decimalValue = 0;
+    for (char c : binaryStr) {
+        decimalValue *= 2;  // Shift left by 1 bit (multiply by 2)
+        if (c == '1') decimalValue += 1;
+        else if (c == '0') continue;
+        else {
+            cerr << "Invalid binary character: " << c << endl;
+            return -1;  // Error handling
+        }
+    }
+    return decimalValue;
+}
+
+
+
+
 int main() {
     string binaryStr;  // Example binary string
 
