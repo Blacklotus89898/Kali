ef98b3cc4b051b84459387094892f3cfec5d5cfd
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2025-09-13 20:25:54 -0400

    angerry

---
+++ cf/pangram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <cctype>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    set<char> letters;
+    for (char c : s) {
+        letters.insert(tolower(c));  // normalize to lowercase
+    }
+
+    if ((int)letters.size() == 26)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
