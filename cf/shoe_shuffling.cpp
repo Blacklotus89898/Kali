6f567ae1c878ec57c1f6457d36585b48ebd98907
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2025-09-14 22:17:30 -0400

    tip: dont' break in input reading,1 pass only vector

--- cf/shoe_shuffling.cpp
+++ cf/shoe_shuffling.cpp
@@ -15,29 +15,48 @@ int main()
         std::cin >> n;
  
         std::vector<ll> v;
-        std::unordered_map<ll, int> m;
+        // std::unordered_map<ll, int> m;
         std::vector<int> res;
- 
+        
+        ll last = -1;
+        ll counter = 0;
+        
+        bool invalid = false;
         for (int i = 0; i < n; ++i)
         {
             ll x = 0;
             std::cin >> x;
+            
+            if (last != x) {
+                if (counter == 1) {
+                    invalid = true;
+                    break;// will cause input desync
+                }
+                counter = 0;
+            }
+
             v.push_back(x);
-            m[x]++;
+            last = x;
+            counter++;
+
+        
+            // m[x]++;
+
+
             res.push_back(i + 1);
         }
+
  
-        bool invalid = false;
-        for (const auto& p: m)
-        {
-            if (p.second == 1)
-            {
-                invalid = true;
-                break;
-            }
-        }
+        // for (const auto& p: m)
+        // {
+        //     if (p.second == 1)
+        //     {
+        //         invalid = true;
+        //         break;
+        //     }
+        // }
  
-        if (invalid)
+        if (invalid || counter == 1)
         {
             std::cout << -1 << std::endl;
         }
