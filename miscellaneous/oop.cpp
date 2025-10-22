bfb8a8d80ff16010ae10b49ca35dbcad559403c1
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2025-10-21 20:57:56 -0400

    oop review in c++ + string tokenizing

--- miscellaneous/oop.cpp
+++ miscellaneous/oop.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 class Rectangle {
 private:
@@ -18,9 +20,45 @@ public:
     }
 };
 
+
+class PracticeClass {
+
+    private:
+    int classId;
+    int number;
+    std::string className;
+
+    public:
+        // constructor
+        PracticeClass(std::string str) {
+            std::vector<std::string> v;
+            std::string curr = "";
+            for (int i = 0; i < str.size(); ++i) {
+                if (str[i] != ' ') {
+                    curr += str[i];
+                } else {
+                    v.push_back(curr);
+                    curr = "";
+                }
+            }
+
+            classId = std::stoi(v[0]);            
+            number = std::stoi(v[1]);            
+
+            for (const std::string& s: v) {
+                std::cout << s ;
+            }
+            std::cout << std::endl;
+
+
+        }
+};
+
 int main() {
     Rectangle rect(10, 20);  // Now passing arguments to the constructor
     rect.display();          // This will display the initialized values
     return 0;
 }
+
+
 
